3914cdebc2264e04c4737ffb1a27098b8edbb274
rnsrk <robert@nasarek.org>
2024-10-30 17:05:40 +0100

    test import onto

--- entrypoint.sh
+++ entrypoint.sh
@@ -93,7 +93,7 @@ else
       drush recipe ../recipes/wisski_${FLAVOUR}
       composer unpack soda-collection-objects-data-literacy/wisski_${FLAVOUR}
       drush wisski-core:recreate-menus
-      drush wisski-core:import-ontology --store="default" --ontology_url="http://wiss-ki.eu/ontology/1.2.0/" --reasoning
+      #drush wisski-core:import-ontology --store="default" --ontology_url="http://wiss-ki.eu/ontology/1.2.0/" --reasoning
       drush cr
     } 1> /dev/null
     echo -e "\033[0;32mWISSKI ${FLAVOUR} RECIPE APPLIED.\033[0m\n"
