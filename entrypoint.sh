e4ae3d80141912300edac00e75f3208dff3c4125
rnsrk <robert@nasarek.org>
2025-06-10 16:09:25 +0200

    new ontology

--- entrypoint.sh
+++ entrypoint.sh
@@ -162,7 +162,7 @@ else
   echo -e "\033[0;32mDEFAULT TRIPLESTORE ADAPTER INSTALLED.\033[0m\n"
 
   echo -e "\033[0;33mIMPORT WISSKI DEFAULT ONTOLOGY.\033[0m"
-  drush wisski-core:import-ontology --store="default" --ontology_url="http://scs.sammlungen.io/ontology/base_record/current/" --reasoning
+  drush wisski-core:import-ontology --store="default" --ontology_url="http://ontologies.sammlungen.io/default/current/" --reasoning
   echo -e "\033[0;32mWISSKI DEFAULT ONTOLOGY IMPORTED.\033[0m\n"
 
   # Apply WissKI Sweet flavour recipe
