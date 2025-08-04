c031cae4cc5e58a7141f65babdf4f80b2cf265ff
rnsrk <robert@nasarek.org>
2025-08-04 08:51:14 +0200

    chfork repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -186,8 +186,8 @@ else
   # Apply WissKI Default Data Model recipe
   echo -e "\033[0;33mAPPLY WISSKI DATA DEFAULT MODEL RECIPE.\033[0m"
  # {
-    composer config repositories.conditional_fields-3495402 vcs https://git.drupalcode.org/issue/conditional_fields-3495402.git
-    composer require 'drupal/wisski_default_data_model:1.x-dev'
+    composer config repositories.1 git https://git.drupalcode.org/issue/conditional_fields-3495402.git
+    composer require 'drupal/wisski_default_data_model:1.0.x-dev'
     drush cr
     drush recipe ../recipes/wisski_default_data_model
     drush wisski-core:recreate-menus
