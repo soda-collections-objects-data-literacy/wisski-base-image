df591c9ac2e9508359cfc0292253237579cc6b8a
rnsrk <robert@nasarek.dev>
2025-11-27 22:30:52 +0100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -267,7 +267,7 @@ EOF
 
   # Install development modules.
   echo -e "\033[0;33mINSTALL DEVELOPMENT MODULES.\033[0m"
-  {
+  
     # Drush command for openid_connect is not implement in main branch yet, so we have to use the fork.
     # Use the fork of openid_connect with drush commands implementation.
     # Need WissKI User Administration module, to check if keycloak groups are matching.
@@ -277,7 +277,6 @@ EOF
     composer require 'drupal/openid_connect:dev-3516375-implement-drush-commands' --prefer-source
     drush en devel health_check project_browser automatic_updates openid_connect sso_bouncer redis -y
 
-  } 1> /dev/null
   echo -e "\033[0;32mDEVELOPMENT MODULES INSTALLED.\033[0m\n"
 
   # Create WissKI User Role.
