ec42c662ad60a93ce5aeb52d74bf11668ec1c9ec
rsnrk <robert@nasarek.dev>
2026-02-04 10:36:51 +0100

    default admin account

--- entrypoint.sh
+++ entrypoint.sh
@@ -139,7 +139,7 @@ else
   if timeout 300 drush si \
     --db-url="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" \
     --site-name="${DRUPAL_SITE_NAME}" \
-    --account-name="${DRUPAL_USER}" \
+    --account-name="admin" \
     --account-pass="${DRUPAL_PASSWORD}" \
     --locale="${DRUPAL_LOCALE}" \
     --yes 2>&1; then
